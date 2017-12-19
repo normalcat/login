eea6922ee3d3ced294d88ad25fcdac4720a7e1d6
Chaoi Chen <chaoichen@icloud.com>
2017-12-18 19:27:34 -0800

    update one title

--- registration/settings.py
+++ registration/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!6tglp^gs-yz)iv^de=hhog$(a%c$*^5aqf$)n24_)$(7^@*3*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.217.165.236']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
